a8f9a2ce08f6937d1c008205b5d051f3d191829b
AntekX <antoliok@hotmail.com>
2020-01-01 18:45:48 +0100

    VulkanSandbox (WIP)

--- Source/SolutionVS/Demo/sample_vs.hlsl
+++ Source/SolutionVS/Demo/sample_vs.hlsl
@@ -1,56 +1,5 @@
 
-#if (0)
-
-// DX12 sandbox sample
-
-static const uint InstanceCount = 24;
-cbuffer Common : register(b0)
-{
-	float4x4 Transform[InstanceCount];
-	float4 Desc;
-};
-
-struct VS_INPUT
-{
-	float3 pos	: POSITION;
-	float4 col	: COLOR0;
-	float2 uv	: TEXCOORD0;
-};
-
-struct PS_INPUT
-{
-	float4 pos	: SV_POSITION;
-	float4 col	: COLOR0;
-	float2 uv	: TEXCOORD0;
-};
-
-static const uint ColorsCount = 7;
-static const float3 Colors[ColorsCount] = {
-	{ 0, 0, 1 },
-	{ 0, 1, 0 },
-	{ 0, 1, 1 },
-	{ 1, 0, 0 },
-	{ 1, 0, 1 },
-	{ 1, 1, 0 },
-	{ 1, 1, 1 }
-};
-
-PS_INPUT main(VS_INPUT input, uint instanceID : SV_InstanceID, uint vertexID : SV_VertexID)
-{
-	PS_INPUT output;
-
-	output.pos = mul(Transform[instanceID + uint(Desc.x)], float4(input.pos.xyz, 1.0f));
-	output.col = input.col;
-	//output.col.xyz = Colors[(instanceID + vertexID) % ColorsCount] * 0.5 + 0.5;
-	//output.col.xyz = Colors[uint(Desc.x / (InstanceCount / 3))];
-	output.col.xyz = 1.0;
-	
-	output.uv = input.uv;
-
-	return output;
-}
-
-#else
+#if defined(VK_SAMPLE)
 
 // Vulkan sandbox sample
 
@@ -110,6 +59,57 @@ PS_INPUT main(VS_INPUT input, uint vertexID : SV_VertexID, uint instanceID : SV_
 
 	output.uv.x = input.pos.x + 0.5;
 	output.uv.y = 0.5 - input.pos.y;
+
+	return output;
+}
+
+#else
+
+// DX12 sandbox sample
+
+static const uint InstanceCount = 24;
+cbuffer Common : register(b0)
+{
+	float4x4 Transform[InstanceCount];
+	float4 Desc;
+};
+
+struct VS_INPUT
+{
+	float3 pos	: POSITION;
+	float4 col	: COLOR0;
+	float2 uv	: TEXCOORD0;
+};
+
+struct PS_INPUT
+{
+	float4 pos	: SV_POSITION;
+	float4 col	: COLOR0;
+	float2 uv	: TEXCOORD0;
+};
+
+static const uint ColorsCount = 7;
+static const float3 Colors[ColorsCount] = {
+	{ 0, 0, 1 },
+	{ 0, 1, 0 },
+	{ 0, 1, 1 },
+	{ 1, 0, 0 },
+	{ 1, 0, 1 },
+	{ 1, 1, 0 },
+	{ 1, 1, 1 }
+};
+
+PS_INPUT main(VS_INPUT input, uint instanceID : SV_InstanceID, uint vertexID : SV_VertexID)
+{
+	PS_INPUT output;
+
+	output.pos = mul(Transform[instanceID + uint(Desc.x)], float4(input.pos.xyz, 1.0f));
+	output.col = input.col;
+	//output.col.xyz = Colors[(instanceID + vertexID) % ColorsCount] * 0.5 + 0.5;
+	//output.col.xyz = Colors[uint(Desc.x / (InstanceCount / 3))];
+	output.col.xyz = 1.0;
+	
+	output.uv = input.uv;
 
 	return output;
 }
